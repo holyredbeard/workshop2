282ad4b9dde2a3504143ec82ed16d74beb191929
Per Magnusson <pm222br@student.lnu.se>
2012-10-17 17:38:34 +0200

    github app sync.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php error_reporting(E_ALL);
+<?php //error_reporting(E_ALL);
 
     // Database
     require_once('Database/DBConfig.php');
@@ -10,17 +10,14 @@
 
     // Views
     require_once('View/MemberView.php');
-<<<<<<< HEAD
     require_once('Controller/RegisterBoatController.php');
     require_once('Controller/RemoveBoatController.php');
-=======
     require_once('View/RegisterView.php');
     require_once('View/CompositionView.php');
 
     // Controllers
     require_once('Controller/RegisterController.php');
     require_once('Controller/MemberController.php');
->>>>>>> a461b2693b41ce1227f70294ef762682815bf13a
 
 class MasterController {
     public function DoControl() {
@@ -32,7 +29,6 @@ class MasterController {
         $regController = new \Controller\RegisterController();
         $memberController = new \Controller\MemberController();
 
-<<<<<<< HEAD
         $regBoatController = new \Controller\RegisterBoatController();
         $removeBoatController = new \Controller\RemoveBoatController();
 
@@ -41,14 +37,12 @@ class MasterController {
         $out .= $regController->DoControl($db);
         $out .= $regBoatController->DoControl($db);
         $out .= $removeBoatController->DoControl($db);
-=======
         $HTMLMember .= $memberController->DoControl($db);
         $HTMLReg .= $regController->DoControl($db);
 
         $cv = new \View\CompositionView();
 
         $out = $cv->merge($HTMLMember, $HTMLReg);
->>>>>>> a461b2693b41ce1227f70294ef762682815bf13a
         
         // kill DB-conn
         $db->Close();
